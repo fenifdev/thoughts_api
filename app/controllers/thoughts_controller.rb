8febef171e4c598492bfaa97ee1a67e5f15d97ee
Federico Nicolás Flores <flores.federico.nicolas@gmail.com>
2019-10-08 17:31:28 -0300

    ADD: Return with status: :ok in success responses

--- app/controllers/thoughts_controller.rb
+++ app/controllers/thoughts_controller.rb
@@ -1,12 +1,12 @@
 class ThoughtsController < ApplicationController
   def index
     thoughts = Thought.all
-    render json: { data: thoughts }
+    render json: { data: thoughts }, status: :ok
   end
 
   def show
     thought = Thought.find(params[:id])
-    render json: { data: thought }
+    render json: { data: thought }, status: :ok
 
     rescue ActiveRecord::RecordNotFound
         render json: { error: "Thought does not exist" }, status: :not_found
